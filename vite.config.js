5f63c53b39c39de6a4a4c7b36c54e0b7fb9f3bc0
Joel Denning <joeldenning@gmail.com>
2020-10-23 17:12:08 -0600

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,8 @@ export default {
   configureServer: ({ app }) => {
     app.use(cors({ origin: '*' }));
   },
-  rollupInputOptions: {
-    entry: 'src/main.js'
-  },
   rollupOutputOptions: {
     format: 'system',
-    file: 'dist/_assets/index.js'
   }
   // This will fix the logo URL problem,
   // but is waiting on https://github.com/vuejs/vue-next/pull/2477
